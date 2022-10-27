d6691d6f0f34a3af35614438be1e7eaa468ae5eb
NFRKT <115152294+NFRKT@users.noreply.github.com>
2022-10-27 12:24:38 +0200

    Create Zadanko.java

---
+++ Zadanko.java
@@ -0,0 +1,38 @@
+package zadanko;
+
+import java.util.Random;
+
+public class Zadanko {
+
+    public static void main(String[] args) {
+        
+        Random dice = new Random();     //obiekt losujący
+        
+        Player gracz = new Player();
+        
+        try {
+            gracz.setName("Al");
+        } catch (IllegalArgumentException ex) {
+            System.err.println("Błąd! " + ex.getMessage());
+        }
+
+        int number;                     //wylosowana liczba
+        int guess;                     //propozycja (strzał) gracza
+        do {
+            System.out.println("---------------------");
+            guess = gracz.guess();
+            number = dice.nextInt(6) + 1;
+            System.out.println("Kostka: " + number);
+
+            System.out.println(gracz.getName() + " : " + guess);
+
+            if (number != guess) {
+                System.out.println("PUDŁO!");
+            } else {
+                System.out.println(gracz.getName() + " BRAWO!");
+            }
+
+        } while (number != guess);
+    }
+
+}
